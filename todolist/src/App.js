a4efea3a556f630438d0af50cc829b1287307eab
okami315 <marchalcobosivan@gmail.com>
2023-01-25 19:18:59 +0100

    cambios_bootstrap

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import FormLogin from './components/FormLogin';
+import FormRegister from './components/FormRegister';
 
 function App() {
   return (
-    <FormLogin/>
+    <FormRegister/>
   );
 }
 
--- todolist/src/components/FormRegister.jsx
+++ todolist/src/components/FormRegister.jsx
@@ -2,73 +2,100 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const FormRegister = () => {
-    const [entradas, setEntradas] = useState([]);
-    
-    const {register,handleSubmit,formState:{ errors } } = useForm();
-    // register pasa todos los nombres del formulario y las restricciones.
-    
-    const onSubmit = (data,e) => {
-        console.log(data);
-        // Limpiamos los componentes
-        e.target.reset();
-        // Guardamos en entradas lo de entradas + data
-        setEntradas([...entradas,data]);
-        console.log(entradas);
-        // muestra el anterior ya que pinta el componente despues de esto ; es diferido
-    }
+  // ust --> useStateSnip...
+  const [entradas, setEntradas] = useState([]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  // register pasa todos los nombres del formulario y las restricciones.
+
+  const onSubmit = (data, e) => {
+    console.log(data);
+    // Limpiamos los componentes
+    e.target.reset();
+    // Guardamos en entradas lo de entradas + data
+    setEntradas([...entradas, data]);
+    console.log(entradas);
+    // muestra el anterior ya que pinta el componente despues de esto ; es diferido
+  };
 
   return (
-    <div className="container my-5">
-      <h2 className="mb-3">Iniciar sesión</h2>
-      <form  onSubmit={handleSubmit(onSubmit)} className="mb-5">
-        <input
-          placeholder="Ingrese nombre de usuario..."
-          className="form-control mb-2"
-          name="username"
-          {
-            // Los pone los valores del array alineados. Los pondrá como propiedades
-            ...register("username",
-            {
-                required : {value:"true", message: "Campo username requerido"}
-            }
-            )
-          }
-        />
-        <span className="text-danger text-small d-bloc mb-2">
-            {errors?.username?.message}
-            {/* Hay errores? es del campo username? muestralo */}
-        </span>
-        <input
-          placeholder="Ingrese correo..."
-          className="form-control mb-2"
-          name="email"
-          {
-            // Los pone los valores del array alineados. Los pondrá como propiedades
-            ...register("email",
-            {
-                required : {value:"true", message: "Campo username requerido"},
-                pattern : {value : /^[A-Z0-9._%+-]+@[A-Z.-]+\.[A-Z]{2,4}$/i , message: "Campo email no valido"}
-            }
-            )
-          }
-        />
-        <input
-          type="password"
-          placeholder="Ingrese contraseña..."
-          className="form-control mb-2"
-          name="password"
-          {
-            // Los pone los valores del array alineados. Los pondrá como propiedades  
-            ...register("password",
-            { 
-                required : {value:"true", message: "Campo password requerido"}, 
-                minLength :4
-            }
-            )    
-          }
-        />
-        <button type="submit" className="btn btn-primary mt-4">Enviar</button>
-      </form>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4 offset-md-4">
+          <form onSubmit={handleSubmit(onSubmit)} className="mb-5">
+            <h2 className="text-center">Registrar usuario</h2>
+            <div className="form-group">
+            <label for="username">Usuario</label>
+            <input
+              placeholder="Ingrese nombre de usuario..."
+              className="form-control"
+              name="username"
+              {
+                // Los pone los valores del array alineados. Los pondrá como propiedades
+                ...register("username", {
+                  required: {
+                    value: "true",
+                    message: "Campo username requerido",
+                  },
+                })
+              }
+            />
+            <span className="text-danger text-small text-center">
+              {errors?.username?.message}
+              {/* Hay errores? es del campo username? muestralo */}
+            </span>
+            </div>
+            <div className="form-group">
+            <label for="password">Contraseña</label>
+            <input
+              placeholder="Ingrese correo..."
+              className="form-control mb-2"
+              name="email"
+              {
+                // Los pone los valores del array alineados. Los pondrá como propiedades
+                ...register("email", {
+                  required: {
+                    value: "true",
+                    message: "Campo username requerido",
+                  },
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z.-]+\.[A-Z]{2,4}$/i,
+                    message: "Campo email no valido",
+                  },
+                })
+              }
+            />
+            <span className="text-danger text-small text-center">
+              {errors?.email?.message}
+              {/* Hay errores? es del campo email? muestralo */}
+            </span>
+            </div>
+            <input
+              type="password"
+              placeholder="Ingrese contraseña..."
+              className="form-control mb-2"
+              name="password"
+              {
+                // Los pone los valores del array alineados. Los pondrá como propiedades
+                ...register("password", {
+                  required: {
+                    value: "true",
+                    message: "Campo password requerido",
+                  },
+                  minLength: 4,
+                })
+              }
+            />
+            <button type="submit" className="form-group text-center">
+              Enviar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
